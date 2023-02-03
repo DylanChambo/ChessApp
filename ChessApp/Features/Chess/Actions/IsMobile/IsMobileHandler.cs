7f49b54d7f1c9116225ae3e80e4f5a7478347f23
DylanChambo <dylan.chambo@gmail.com>
2023-02-03 16:07:26 +1300

    feat: :sparkles: can move on mobile

---
+++ ChessApp/Features/Chess/Actions/IsMobile/IsMobileHandler.cs
@@ -0,0 +1,22 @@
+﻿using BlazorState;
+using ChessApp.Data;
+using MediatR;
+
+namespace ChessApp.Features.Chess;
+
+public partial class ChessState
+{
+    public class IsMobileHandler : ActionHandler<IsMobileAction>
+    {
+        public IsMobileHandler(IStore store): base(store) { }
+
+        ChessState chessState => Store.GetState<ChessState>();
+
+        public override Task<Unit> Handle(IsMobileAction isMobileAction, CancellationToken cancellationToken)
+        {
+            chessState.Mobile = isMobileAction.Mobile;
+            return Unit.Task;
+        }
+    }
+}
+
