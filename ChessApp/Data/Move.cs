fea1f24de7c9701905270244c31b07330df6de4d
DylanChambo <dylan.chambo@gmail.com>
2023-02-08 15:47:20 +1300

    feat: :sparkles: add move flags

--- ChessApp/Data/Move.cs
+++ ChessApp/Data/Move.cs
@@ -1,19 +1,33 @@
-﻿namespace ChessApp.Data
-{
-    public struct Move
-    {
-        public Position StartSquare;
-        public Position TargetSquare;
-        public Move(Position startSquare, Position targetSquare)
-        {
-            StartSquare = startSquare;
-            TargetSquare = targetSquare;
-        }
+﻿namespace ChessApp.Data;
 
-        public Move()
-        {
-            StartSquare = new Position();
-            TargetSquare = new Position();
-        }
+public struct Move
+{
+    public Position StartSquare;
+    public Position TargetSquare;
+    public MoveFlag MoveFlag;
+    public Move(Position startSquare, Position targetSquare, MoveFlag moveFlag = MoveFlag.None)
+    {
+        StartSquare = startSquare;
+        TargetSquare = targetSquare;
+        MoveFlag = moveFlag;
     }
+
+    public Move()
+    {
+        StartSquare = new Position();
+        TargetSquare = new Position();
+        MoveFlag = MoveFlag.None;
+    }
+}
+
+public enum MoveFlag
+{
+    None,
+    EnPassant,
+    PawnDoubleMove,
+    Castling,
+    PromoteToQueen,
+    PromoteToKnight,
+    PromoteToRook,
+    PromoteToBishop
 }
